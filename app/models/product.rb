2eabc6849705deb61d24cd2d2b626353594f9241
Ha Le <ha.le@eastagile.com>
2016-04-16 01:49:03 +0700

    catalog display

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,15 @@ class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
-	validates :title, length: { minimum: 10 }
+	# validates :title, length: { minimum: 10 }
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+	validates_length_of :title, minimum:10, too_short: "greater than 10 characters if you dont mind"
+
+	def self.latest
+		Product.order(:updated_at).last
+	end
 end
